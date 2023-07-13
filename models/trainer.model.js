564421253fe8bd7fdfa83d607e1e15a374716675
Lacey Washington <lacey.m.washington@gmail.com>
2023-07-12 21:35:44 -0500

    added trainers

---
+++ models/trainer.model.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// creates trainers model for  for pokemon.
+const Trainer = (dbConnection, DataTypes) => {
+  return dbConnection.define('trainer', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    homeTown: {
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    badges:{
+      type:DataTypes.INTEGER,
+      allowNull: true
+    }
+  });
+};
